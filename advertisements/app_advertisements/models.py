a962f62d8b058743450ab6d69a863bf65f6364c3
Egor <egorushka_baklazhov01@mail.ru>
2023-08-02 12:21:03 +0300

    ДЗ занятия 7

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+# Заголовок
+    title = models.CharField("Заголовок", max_length=128)
+
+# Описание
+    description= models.TextField("Описание")
+# Цена
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+
+# Дата создания
+    created_at = models.DateTimeField(auto_now_add=True)
+# Дата изменения
+    updated_at = models.DateTimeField(auto_now=True)
+# Торг
+    auction = models.BooleanField("Торг", help_text="Ответьте, если хотите торговаться")
+
+    class Meta:
+        db_table = 'advertisements'
+# Изображение
+
+# Адрес
+
+# Отзывы
+
+# Контакты продавца
+
+# Похожие товары
